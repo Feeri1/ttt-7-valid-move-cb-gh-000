8ba834f5953a492729b4336e51b7527e002044db
Franziska Meurs <fiae17l.meurs@mm-bbs.de>
2017-11-15 12:54:51 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,8 +6,8 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def helper_method_for_validmove
   if position_taken?
-    execute something
+
   else
-    execute something else
+    
   end
 end
